099686694180ac2080a7e4926a9bd5ebba73ea72
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2023-05-16 17:08:05 -0300

    feat: init prisma and eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,15 @@
-console.log("Olá");
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server runnings on http://localhost:3333')
+  })
